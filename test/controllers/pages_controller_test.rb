827bd62e5304f908bc9410fac4a5e7c528680905
Grace <gracelidinskysmith@gmail.com>
2022-04-18 10:43:24 -0400

    Add branches
    Set up CI
    
    Add database
    
    Add bunder-audit
    
    Remove bundlr-cache
    
    Remove buggy lint
    
    Remove lint
    
    Add all branches

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
+  test 'should get welcome' do
     get pages_welcome_url
     assert_response :success
   end
