87d922d60a1c53417706f4fecc6dff42a81e3226
Grace <gracelidinskysmith@gmail.com>
2022-04-18 16:05:59 -0400

    WIP commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -8,7 +8,7 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue it's Grace!"
     }
   }
 }
@@ -18,5 +18,6 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+
 }
 </style>
